4c5a07a5e49616044665ec28f6d252b32e9045fe
Jaime-student <136556174+Jaime-student@users.noreply.github.com>
2023-06-23 15:50:24 +1000

    it is done

--- Assets/EnemyScript.cs
+++ Assets/EnemyScript.cs
@@ -1,19 +1,57 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using Random = UnityEngine.Random;
+
 
 public class EnemyScript : MonoBehaviour
 {
-    [SerializeField] private int health = 100;
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] public int enemyHealth = 100;
+    [SerializeField] private int playerAttack = 10;
+
+    private float enemyLimitHealth = 100;
+    public int randomInteger;
+    public int randomIntegerXp;
+
+    
+    private void Start()
     {
+        Debug.Log("An enemy has appeared, attack to gain xp!");
         
     }
+       void Update()
+       {
+        
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                playerAttack = Random.Range(7, 15);
 
-    // Update is called once per frame
-    void Update()
-    {
-       
-    }
+                if (playerAttack > 1)
+                {
+                    enemyHealth = enemyHealth - playerAttack;
+
+                }
+
+
+                if (enemyHealth < 100 && enemyHealth > 0)
+                {
+                    Debug.Log($"Enemy health at " + enemyHealth);
+                }
+                if (enemyHealth < 0)
+                {
+                    randomIntegerXp = Random.Range(15, 40);
+                    Debug.Log("Enemy has been killed, you gained xp amount " + randomIntegerXp);
+                    FindObjectOfType<PlayerCharacter>().UpdatePlayerXp(randomIntegerXp);
+                  if (enemyHealth < 0)
+                  {
+                    enemyHealth = 100;
+                  }
+                    
+                }
+
+            }
+        
+       }
 }
--- Assets/PlayerCharacter.cs
+++ Assets/PlayerCharacter.cs
@@ -1,40 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
+using Unity.VisualScripting;
 using UnityEngine;
 public class PlayerCharacter : MonoBehaviour
 {
     // Variables will go first
-    [SerializeField] private int attack = 10;
-    [SerializeField] private int health = 100;
-    [SerializeField] private int level = 1;
-                     private bool runOnce; 
+    [SerializeField] public int level = 1;
+    [SerializeField] public int xp = 0;
+    private float xpLimit = 100;
+    private bool runOnce;
     // Methods will go underneath here
-
-    // Start is called before the first frame update
-   private void Start()
-    {
-        attack =  10;
-        health = 100;
-        level =    1;
-        runOnce = false;
-    }
     
-    // add a level
-    private void Update()
-    {
+    
 
-        if (runOnce = false);
+        // Start is called before the first frame update
+    private void Start()
+    {
+        Debug.Log("Your name is gabe and you've started this journey in this dungeon, kill enemys and climb to level 5 to win.Press A to start and attack later on");
+        level = 1;
+     }
+
+        // add a level
+        public void UpdatePlayerXp(int killed)
         {
-        if (Input.GetKeyDown(KeyCode.Space))
-        Debug.Log("Your name is gabe and you've started this journey in this dungeon, kill enemys and climb to level 5 to win.");
-        runOnce = true;
+            xp += killed;
+            {
+                if (xp >= xpLimit)
+                {
+                    Debug.Log("You have reached level " + level);
+                    level = level + 1;
+                    if (xp > 101)
+                    {
+                        xp = 0;
+                    }
+                    if (level >= 6)
+                    {
+                    Debug.Log("You have reached level 5 and are strong enough to leave the dungeon, congratulations");
+                    }
+                
+                }
+            }
         }
-        
-        if (runOnce = true);
-        {
-         if  (Input.GetKeyDown(KeyCode.Space))  
-         Debug.Log("An enemy has appeared, attack to gain xp!");
-        }
-        
-    }
 }
+ 
